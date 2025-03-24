1e492e9bd085181db5640660ea003ce1cacc842d
Paige Feldmann <paigeafeldmann@gmail.com>
2025-03-24 13:33:12 -0500

    updated server.ts

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -10,12 +10,16 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // DONE: Serve static files of entire client dist folder
-app.use(express.static('client/dist'));
+app.use(express.static('../client/dist'));
 // DONE: Implement middleware for parsing JSON and urlencoded form data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // TODO: Implement middleware to connect the routes
 app.use(routes);
+app.get("*", (_req, res) => {
+    res.sendFile('index.html', { root: '../client/dist' })
+    });
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+
